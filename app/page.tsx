fad4410b13d0c07deddcdceeb2fb68e259748058
deucenn <panix.black@web.de>
2024-04-18 15:59:54 +0200

    first init

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Room } from "./Room";
+import Live from "@/components/Live";
+
+export default function Page() {
+  return (
+    <div>
+      <Live />
+    </div>
+  );
+}
---
+++ components/cursor/CursorChat.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CursorChat() {
+  return (
+    <div>CursorChat</div>
+  )
+}
+
+export default CursorChat
